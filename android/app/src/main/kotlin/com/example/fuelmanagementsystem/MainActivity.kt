3af032535fb01c14aeb7f189dc6c08f94e6f1ef1
NisalPerera1 <nisaldilesh14@gmail.com>
2023-01-12 14:47:27 +0530

    Flutter UI for User Registration and User Login

---
+++ android/app/src/main/kotlin/com/example/fuelmanagementsystem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fuelmanagementsystem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:fuelmanagementsystem/screens/login_screen/login_screen.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  get kPrimaryColor => null;
+
+  get kBackgroundColor => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kPrimaryColor,
+              fontFamily: 'Montserrat',
+            ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
